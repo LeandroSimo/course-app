d186e548835fe7f49361a4149baa296892c2ce5d
LeandroSimo <leo.bt.12@hotmail.com>
2023-12-06 16:26:43 -0300

    feat: adjustments of the alert dialog

--- lib/app/modules/student/presentation/ui/student_page.dart
+++ lib/app/modules/student/presentation/ui/student_page.dart
@@ -84,16 +84,7 @@ class StudentPageState extends State<StudentPage> {
                                 firstName: firstNameController.text,
                                 lastName: lastNameController.text,
                               ),
-                              func: () {
-                                return showDialog(
-                                  context: context,
-                                  builder: (context) {
-                                    return const AlertDialog(
-                                      content: Text("Aluno já cadastrado"),
-                                    );
-                                  },
-                                );
-                              },
+                              func: _showAlertDialog(context, _size),
                             );
                             Navigator.pop(context);
                             lastNameController.clear();
@@ -150,6 +141,51 @@ class StudentPageState extends State<StudentPage> {
           );
         },
       ),
+    );
+  }
+
+  _showAlertDialog(BuildContext context, Size size) {
+    showDialog(
+      context: context,
+      builder: (context) {
+        return AlertDialog(
+          content: Container(
+            height: size.height * 0.2,
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.start,
+              children: [
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Icon(
+                      Icons.warning_amber_rounded,
+                      color: Theme.of(context).colorScheme.primary,
+                      size: 40,
+                    ),
+                    IconButton(
+                      onPressed: () => Navigator.pop(context),
+                      icon: const Icon(
+                        Icons.close,
+                        color: Colors.black,
+                      ),
+                    ),
+                  ],
+                ),
+                const Spacer(),
+                const Text(
+                  'Aluno já cadastrado!',
+                  style: TextStyle(
+                    fontSize: 24,
+                    fontWeight: FontWeight.w500,
+                    color: Colors.black,
+                  ),
+                ),
+                const Spacer(),
+              ],
+            ),
+          ),
+        );
+      },
     );
   }
 }
